d81ff85720b9d38948ca38c116ddc0015bc6330a
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2019-11-22 23:33:02 +0100

    hi

---
+++ Battleship/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public partial class Form1 : Form
+    {
+        bool isShipPPlaced = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void a1_MouseClick(object sender, MouseEventArgs e)
+        {
+            a1.Text = "";
+            a1.BackColor = Color.Green;
+        }
+    }
+}
